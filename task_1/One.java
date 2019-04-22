fdd059bab6b3e791e076ffb8e9a68442347ddcb6
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-22 09:15:15 +0300

    Update One.java

--- task_1/One.java
+++ task_1/One.java
@@ -2,7 +2,9 @@ package hw_3.task_1.task_1;
 
 public class One {
         public static void main(String[] args) {
+                // имена переменных должны быть говорящими, никаких 'i', например, inputNumber
             int i = Integer.parseInt(args[0]);
+                // лучше не использовать русские слова для имен переменных
             int otvet =  (i<<8);
             System.out.println(otvet);
         }
