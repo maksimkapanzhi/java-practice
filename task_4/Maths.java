c5a1cd33c940147362f6f4cae941098d78523c0c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-22 09:20:03 +0300

    Update Maths.java

--- task_4/Maths.java
+++ task_4/Maths.java
@@ -1,9 +1,12 @@
+// странное имя пакета и вряд ли оно соответстует реальности
 package hw_3.task_1.task_4;
 
 import static java.lang.Math.cos;
 
 public class Maths {
     public static void main(String[] args) {
+        // думаю пару лишних скобок здесь точно есть
+        // имя переменной
        double a = (((( (cos(180)) + (256 / 8) - (Math.pow(2, 3) ))* Math.PI)));
         System.out.println(a);// в градусы не переводил, поскольку в задании не указано, что такое 180 - градусы или радианы
     }
