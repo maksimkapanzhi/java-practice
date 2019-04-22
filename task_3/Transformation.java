7a65c9a8a21e752a19dda1408b6b10728b75d3f2
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-22 09:17:19 +0300

    Update Transformation.java

--- task_3/Transformation.java
+++ task_3/Transformation.java
@@ -3,6 +3,8 @@ package hw_3.task_1.task_3;
 public class Transformation {
     public static void main(String[] args) {
         String exampleString = ("Whara is my dog+");
+        
+        //можно записать в одну строку exampleString.replace('a', 'e').replace('+', '?')
         String newString = exampleString.replace('a', 'e');
         String finalString = newString.replace('+', '?');
         System.out.println(finalString);
