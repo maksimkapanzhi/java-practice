3ad267376bee57ccd038aeeff06a5857918496fd
maksimkapanzhi <maksim.kapanzhi@gmail.com>
2019-05-11 23:23:21 +0300

    Homework abour car

---
+++ HW_5/Mycar.java
@@ -0,0 +1,12 @@
+package hw_for1305;
+
+public class Mycar {
+    public static void main(String[] args) {
+        Car car = new Car(0, "Off");
+        car.turnOnEngine();
+        car.transUp();
+        car.transUp();
+        car.goToHell();
+    }
+
+}
