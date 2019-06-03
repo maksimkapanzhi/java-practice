fca72ee240dae48a2db0c337b3c992ae81580223
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-06-03 11:24:39 +0300

    Update Words.java

--- hw_11/task_1/Words.java
+++ hw_11/task_1/Words.java
@@ -14,6 +14,14 @@ public class Words {
         words.add("Mouse");
         words.add("Mouse");
         System.out.println(words);
+        /*
+        1. Тебе не нужен здесь LinkedHashSet (он сохраняет порядок добавления элементов)
+        2. если ты зайдешь в конструктор LinkedHashSet, который принимает коллекцию,
+        то там вызывается метод addAll() и затем через итератор добавляются элементы
+        Выводы:
+        1. HashSet будет достаточно
+        2. Можно и самому все добавить
+        */
         Set<String> set=new LinkedHashSet<>(words);
         System.out.println(set);
     }
